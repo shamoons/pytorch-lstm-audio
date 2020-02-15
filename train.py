45261fc328eddcd333a5b7307b0ad2345287dde3
Shamoon Siddiqui <shamoons@users.noreply.github.com>
2020-02-15 11:50:35 -0500

    Basic Model Working (#1)
    * Added raw audio
    
    * Dataset
    
    * Simplified
    
    * Train
    
    * Less training data
    
    * Changed to mean
    
    * Adding a decoder
    
    * SimpleRNN functional
    
    * Learned on one sample of training data
    
    * Got a basic model working

---
+++ train.py
@@ -0,0 +1,49 @@
+import torch
+import torch.optim as optim
+from audiodataset import AudioDataset
+from lstm import MyLSTMEncoder
+
+
+def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    train_set = AudioDataset(
+        'data/dev-noise-subtractive-250ms-1/', train_set=True)
+
+    kwargs = {'num_workers': 1, 'pin_memory': True} if device == 'cuda' else {}
+
+    train_loader = torch.utils.data.DataLoader(
+        train_set, batch_size=1, shuffle=True, **kwargs)
+
+    model = MyLSTMEncoder(input_size=5)
+    optimizer = optim.Adam(model.parameters(), lr=0.01, weight_decay=0.0001)
+    loss_fn = torch.nn.MSELoss(reduction='sum')
+
+    for epoch in range(300):
+        for i, data in enumerate(train_loader):
+            inputs = data[0]
+            outputs = data[1]
+
+            print('inputs', inputs, inputs.size())
+            print('outputs', outputs, outputs.size())
+            optimizer.zero_grad()
+
+            pred = model(inputs)
+            print('pred', pred[0], pred[0].size())
+
+            loss = loss_fn(pred[0], outputs)
+
+            model.zero_grad()
+
+            loss.backward()
+            optimizer.step()
+
+            if i % 10 == 0:
+                print('Epoch: ', epoch, '\ti: ', i, '\tLoss: ', loss)
+
+            print('\n')
+    print('Final Loss: ', loss)
+
+
+if __name__ == '__main__':
+    main()
