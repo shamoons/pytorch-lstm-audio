777a731e4d0181cedf3973f66abf1ba9578daf0b
Shamoon Siddiqui <shamoon@develop.io>
2020-02-23 11:44:40 -0500

    Trying bidirectional and log

--- audio_util.py
+++ audio_util.py
@@ -3,7 +3,7 @@ from scipy import signal
 import numpy as np
 
 
-def load_audio_spectrogram(audio_path):
+def load_audio_spectrogram(audio_path, eps=1e-10):
     samples, sample_rate = sf.read(audio_path)
     # Taken from https://github.com/PaddlePaddle/DeepSpeech/blob/766e96e600795cea4187123b9ed76dcd250f2d04/data_utils/featurizer/audio_featurizer.py#L121
     nperseg = int(sample_rate * 0.001 * 20)  # 20ms
@@ -15,7 +15,7 @@ def load_audio_spectrogram(audio_path):
     # We swap them here.
     spectrogram = np.swapaxes(spectrogram, 0, 1)
 
-    return spectrogram
+    return np.log(spectrogram + eps)
 
 
 def load_times_frequencies(audio_path):
--- model.py
+++ model.py
@@ -4,12 +4,10 @@ import tensorflow as tf
 
 from tensorflow.keras import optimizers
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-from tensorflow.keras.layers import LSTM, Dense, Activation, Dropout, Bidirectional
+from tensorflow.keras.layers import LSTM, Dense, Activation, Dropout, Bidirectional, BatchNormalization
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.utils import Sequence
-from tensorflow.keras.utils import multi_gpu_model
 from wandb.keras import WandbCallback
-from tensorflow.compat.v1.keras.layers import CuDNNLSTM
 import wandb
 
 
@@ -20,28 +18,30 @@ class SpeechBaselineModel():
         return
 
     def build(self, seq_length, feature_dim, lstm1_size, lstm2_size, lstm3_size, lstm4_size):
-        if tf.test.is_gpu_available(cuda_only=True) == True:
-            LSTMLayer = CuDNNLSTM
-        else:
-            LSTMLayer = LSTM
 
-        self.model.add(LSTMLayer(lstm1_size, input_shape=(
-            seq_length, feature_dim), return_sequences=True))
+        self.model.add(Bidirectional(LSTM(lstm1_size, input_shape=(
+            seq_length, feature_dim), return_sequences=True)))
+        self.model.add(BatchNormalization())
         self.model.add(Dropout(0.2))
-        self.model.add(LSTMLayer(lstm2_size, return_sequences=True))
+
+        self.model.add(Bidirectional(
+            LSTM(lstm2_size, return_sequences=True)))
+        self.model.add(BatchNormalization())
         self.model.add(Dropout(0.2))
-        self.model.add(LSTMLayer(lstm3_size, return_sequences=True))
+
+        self.model.add(Bidirectional(
+            LSTM(lstm3_size, return_sequences=True)))
+        self.model.add(BatchNormalization())
         self.model.add(Dropout(0.2))
-        self.model.add(LSTMLayer(lstm4_size, return_sequences=True))
+
+        self.model.add(Bidirectional(
+            LSTM(lstm4_size, return_sequences=True)))
+        self.model.add(BatchNormalization())
         self.model.add(Dropout(0.2))
+
         self.model.add(Dense(feature_dim, activation='linear'))
 
     def compile(self, learning_rate):
-        try:
-            self.model = multi_gpu_model(self.model)
-        except:
-            pass
-
         adam_optimizer = optimizers.Adam(
             learning_rate=learning_rate, clipnorm=1.0)
         self.model.compile(loss='mean_squared_error', optimizer=adam_optimizer)
